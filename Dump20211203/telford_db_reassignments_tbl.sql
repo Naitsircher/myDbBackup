12a4d675d609fe7678cd0d5655e90a97d0bb076e
Neyt <chanchristianarana@gmail.com>
2021-12-03 15:17:04 +0800

    local m6
    added Vaccine_Status column in masterlist

---
+++ Dump20211203/telford_db_reassignments_tbl.sql
@@ -0,0 +1,60 @@
+-- MySQL dump 10.13  Distrib 8.0.27, for Linux (x86_64)
+--
+-- Host: localhost    Database: telford_db
+-- ------------------------------------------------------
+-- Server version	8.0.27-0ubuntu0.21.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `reassignments_tbl`
+--
+
+DROP TABLE IF EXISTS `reassignments_tbl`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `reassignments_tbl` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `reAssignedTo` varchar(255) DEFAULT NULL,
+  `reAssignedName` varchar(255) DEFAULT NULL,
+  `reAssignedTeam` varchar(45) DEFAULT NULL,
+  `department` varchar(255) DEFAULT NULL,
+  `reAssignDescription` text,
+  `analysis_tbl_id` int NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  KEY `fk_reAssignments_tbl_analysis_tbl1_idx` (`analysis_tbl_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `reassignments_tbl`
+--
+
+LOCK TABLES `reassignments_tbl` WRITE;
+/*!40000 ALTER TABLE `reassignments_tbl` DISABLE KEYS */;
+INSERT INTO `reassignments_tbl` VALUES (1,'1111','Araña, Christian R.','N','MIS','test reassignment.',2,'2021-11-25 09:32:35','2021-11-25 17:32:35'),(2,'1280','Pedrosa, Geralyn S.','A','Process Engineering','sample lang',2,'2021-11-26 03:32:10','2021-11-26 11:32:10'),(3,'1110','Boceron, Ernesto A.','C','Equipment Engineering','Sample',2,'2021-11-26 07:07:39','2021-11-26 15:07:39'),(4,'1111','Araña, Christian R.','N','MIS','sAMPLE',2,'2021-11-26 08:20:17','2021-11-26 16:20:17'),(5,'14516','Paloyo , Vanessa Jane T.','A','Production - RFC','Sample issuance.',2,'2021-12-01 07:46:24','2021-12-01 15:46:24');
+/*!40000 ALTER TABLE `reassignments_tbl` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-12-03 15:11:36
